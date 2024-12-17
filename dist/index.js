be2b540f81fa94db40faca965b46b40cc04cea05
hamdi <hamdi.pirillo4@gmail.com>
2024-12-17 11:55:38 +0100

    added the functionality to look up a path for a filename

--- dist/index.js
+++ dist/index.js
@@ -2,23 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
-const args = process.argv.slice(2);
-readFile();
-function readFile() {
-    for (const arg of args) {
-        const parts = arg.split('.');
-        if (parts.pop() !== 'md') {
-            console.log('Only MD files are allowed!');
-            process.exit(0);
-        }
+if (!process.argv[2]) {
+    console.log('The script does not work without a provided filename.');
+    process.exit(0);
+}
+const cwd = (0, path_1.dirname)(__dirname);
+let path = '';
+const ls = (0, fs_1.readdirSync)(cwd, {
+    encoding: 'utf-8'
+}).filter(path => {
+    const stats = (0, fs_1.statSync)((0, path_1.join)(cwd, path));
+    if (path.startsWith('.'))
+        return;
+    return stats.isDirectory();
+});
+for (let i = 0; i < ls.length; i++) {
+    if ((0, fs_1.existsSync)(path = (0, path_1.join)(cwd, ls[i], process.argv[2]))) {
+        (0, fs_1.createReadStream)(path)
+            .on('error', console.error)
+            .pipe(process.stdout);
+        break;
     }
-    (0, fs_1.createReadStream)((0, path_1.join)('public', ...args))
-        .on('error', (err) => {
-        if (err.code === 'ENOENT') {
-            console.log('No such file exists.');
-            return;
-        }
-        console.error(err);
-    })
-        .pipe(process.stdout);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import { createReadStream } from 'fs'
-import { join } from 'path'
+import { createReadStream, existsSync, readdirSync, statSync } from 'fs'
+import { dirname, join } from 'path'
 
-const args = process.argv.slice(2)
-readFile()
+if (!process.argv[2]) {
+    console.log('The script does not work without a provided filename.')
+    process.exit(0)
+}
 
-function readFile() {
-    for (const arg of args) {
-        const splits = arg.split('.')
+const cwd = dirname(__dirname)
+let path = ''
 
-        if (splits.pop() !== 'md') {
-            console.log('Only MD files are allowed!')
-            process.exit(0)
-        }
+const ls = readdirSync(cwd, {
+    encoding: 'utf-8'
+}).filter(path => {
+    const stats = statSync(join(cwd, path))
+    if (path.startsWith('.')) return
+    return stats.isDirectory()
+})
+
+for (let i = 0; i < ls.length; i++) {
+    if (existsSync(path = join(cwd, ls[i], process.argv[2]))) {
+        createReadStream(path)
+            .on('error', console.error)
+            .pipe(process.stdout)
+
+        break
     }
-
-    createReadStream(join('public', ...args))
-        .on('error', (err: any) => {
-            if (err.code === 'ENOENT') {
-                console.log('No such file exists.')
-                return
-            }
-
-            console.error(err)
-        })
-        .pipe(process.stdout)
 }
