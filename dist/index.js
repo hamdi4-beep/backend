1e6ee820d07c3ba481c7b3de8e202c918e8f3077
hamdi <hamdi.pirillo4@gmail.com>
2024-12-18 08:25:10 +0100

    made some improvements

--- dist/index.js
+++ dist/index.js
@@ -2,33 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
-const filename = (0, path_1.basename)(process.argv[2]);
-if (!filename) {
-    console.log('The filename was not provided.');
-    process.exit(1);
-}
-const cwd = (0, path_1.dirname)(__dirname);
-let path = '', filefound = false;
-const ls = (0, fs_1.readdirSync)(cwd, 'utf-8').filter(item => {
-    if (item.startsWith('.'))
-        return;
-    try {
-        const stats = (0, fs_1.statSync)(item);
-        return stats.isDirectory();
-    }
-    catch (e) {
-        console.error(e);
-    }
-});
 const readFileStream = (path) => (0, fs_1.createReadStream)(path)
     .on('error', console.error)
     .pipe(process.stdout);
-for (const dir of ls) {
-    path = (0, path_1.join)(dir, filename);
-    if (filefound = (0, fs_1.existsSync)(path)) {
-        readFileStream(path);
-        break;
+const path = getFilePath((0, path_1.basename)(process.argv[2]));
+if (!path) {
+    console.log('No such file was found');
+    process.exit(1);
+}
+readFileStream(path);
+function getFilePath(filename) {
+    const cwd = (0, path_1.dirname)(__dirname);
+    let path = (0, path_1.join)(cwd, filename);
+    const ls = (0, fs_1.readdirSync)(cwd, 'utf-8').filter(item => {
+        if (item.startsWith('.'))
+            return;
+        try {
+            const stats = (0, fs_1.statSync)(item);
+            return stats.isDirectory();
+        }
+        catch (e) {
+            console.error(e);
+        }
+    });
+    if ((0, fs_1.existsSync)(path))
+        return (0, path_1.basename)(path);
+    for (const dir of ls) {
+        path = (0, path_1.join)(dir, filename);
+        if ((0, fs_1.existsSync)(path))
+            return path;
     }
 }
-if (!filefound)
-    console.log('No such file was found within the subdirectories.');
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
 import { createReadStream, existsSync, readdirSync, statSync } from 'fs'
 import { basename, dirname, join } from 'path'
 
-const filename = basename(process.argv[2])
-
-if (!filename) {
-    console.log('The filename was not provided.')
-    process.exit(1)
-}
-
-const cwd = dirname(__dirname)
-let path = '', filefound = false
-
-const ls = readdirSync(cwd, 'utf-8').filter(item => {
-    if (item.startsWith('.')) return
-
-    try {
-        const stats = statSync(item)
-        return stats.isDirectory()
-    } catch (e) {
-        console.error(e)
-    }
-})
-
 const readFileStream = (path: string) =>
     createReadStream(path)
         .on('error', console.error)
         .pipe(process.stdout)
 
-for (const dir of ls) {
-    path = join(dir, filename)
+const path = getFilePath(basename(process.argv[2]))
 
-    if (filefound = existsSync(path)) {
-        readFileStream(path)
-        break
-    }
+if (!path) {
+    console.log('No such file was found')
+    process.exit(1)
 }
 
-if (!filefound) console.log('No such file was found within the subdirectories.')
+readFileStream(path)
+
+function getFilePath(filename: string) {
+    const cwd = dirname(__dirname)
+    let path = join(cwd, filename)
+
+    const ls = readdirSync(cwd, 'utf-8').filter(item => {
+        if (item.startsWith('.')) return
+
+        try {
+            const stats = statSync(item)
+            return stats.isDirectory()
+        } catch (e) {
+            console.error(e)
+        }
+    })
+
+    if (existsSync(path)) return basename(path)
+
+    for (const dir of ls) {
+        path = join(dir, filename)
+        if (existsSync(path)) return path
+    }
+}
