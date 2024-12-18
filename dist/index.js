ecee055063cbe851fe84034b2d10388ebc2525d6
hamdi <hamdi.pirillo4@gmail.com>
2024-12-18 15:52:11 +0100

    Modularized the codebase

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
 const stream_1 = require("stream");
-(0, fs_1.createReadStream)((0, path_1.join)('contents', 'style-guide.md'))
-    .on('error', console.error)
-    .pipe(new stream_1.Transform({
+if (!process.argv[2]) {
+    console.log('Provide the filename argument.');
+    process.exit(1);
+}
+const filename = (0, path_1.basename)(process.argv[2]);
+const filterByRegEx = (regex) => new stream_1.Transform({
     transform(chunk, encoding, callback) {
-        var _a;
-        const matches = (chunk + '').match(/^#{2}\s([\S\s]+)$/);
-        callback(null, (_a = matches === null || matches === void 0 ? void 0 : matches[1]) !== null && _a !== void 0 ? _a : '');
+        const splitted = (chunk + '').split('\n');
+        this.push(JSON.stringify(splitted.filter(it => regex.test(it)), null, '\t'));
     }
-}))
+});
+(0, fs_1.createReadStream)(findFilePath(filename))
+    .on('error', handleError)
+    .pipe(filterByRegEx(/-\s(?:[\S\s]+)/))
     .pipe(process.stdout);
+function handleError(err) {
+    if (err.code === 'ENOTENT') {
+        console.log('No such file was found.');
+        process.exit(1);
+    }
+    console.error(err);
+}
+function findFilePath(filename) {
+    const ls = (0, fs_1.readdirSync)((0, path_1.dirname)(__dirname), 'utf8');
+    let path = '';
+    for (const dir of ls) {
+        if (!(0, fs_1.existsSync)((0, path_1.join)(dir, filename)))
+            continue;
+        return (0, path_1.resolve)(dir, filename);
+    }
+    if (!path) {
+        console.log('No such file exists within the subdirectories.');
+        return;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
-import { createReadStream } from 'fs'
-import { join } from 'path'
-import { Transform, TransformCallback } from 'stream'
+import { createReadStream, createWriteStream, existsSync, readdirSync } from 'fs'
+import { basename, dirname, join, resolve } from 'path'
+import { Transform } from 'stream'
 
-createReadStream(join('contents', 'style-guide.md'))
-    .on('error', console.error)
-    .pipe(new Transform({
-        transform(chunk, encoding, callback) {
-            const matches = (chunk + '').match(/^#{2}\s([\S\s]+)$/)
-            callback(null, matches?.[1] ?? '')
-        }
-    }))
+if (!process.argv[2]) {
+    console.log('Provide the filename argument.')
+    process.exit(1)
+}
+
+const filename = basename(process.argv[2])
+
+const filterByRegEx = (regex: RegExp) => new Transform({
+    transform(chunk, encoding, callback) {
+        const splitted = (chunk + '').split('\n')
+        this.push(JSON.stringify(splitted.filter(it => regex.test(it)), null, '\t'))
+    }
+})
+
+createReadStream(findFilePath(filename) as string)
+    .on('error', handleError)
+    .pipe(filterByRegEx(/-\s(?:[\S\s]+)/))
     .pipe(process.stdout)
+
+function handleError(err: any) {
+    if (err.code === 'ENOTENT') {
+        console.log('No such file was found.')
+        process.exit(1)
+    }
+
+    console.error(err)
+}
+
+function findFilePath(filename: string) {
+    const ls = readdirSync(dirname(__dirname), 'utf8')
+    let path = ''
+
+    for (const dir of ls) {
+        if (!existsSync(join(dir, filename))) continue
+        return resolve(dir, filename)
+    }
+
+    if (!path) {
+        console.log('No such file exists within the subdirectories.')
+        return
+    }
+}
