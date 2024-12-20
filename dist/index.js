775185e98934cba83ee40546662f71709a0019fb
hamdi <hamdi.pirillo4@gmail.com>
2024-12-20 14:42:25 +0100

    cleaned up the codebase

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,26 @@
 "use strict";
-var _a, _b;
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
 const stream_1 = require("stream");
-const filename = (0, path_1.basename)((_a = process.argv[2]) !== null && _a !== void 0 ? _a : '');
-if (!filename) {
-    console.error('Expected a filename.');
-    process.exit(1);
-}
-if (((_b = filename.split('.').pop()) === null || _b === void 0 ? void 0 : _b.toLowerCase()) !== 'md') {
-    console.error('Only MD files are allowed.');
-    process.exit(1);
-}
-const filterByRegex = (regex) => new stream_1.Transform({
-    transform(chunk, encoding = 'utf8', callback) {
+const splitTransform = (regex) => new stream_1.Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
         const lines = (chunk + '').split('\n');
-        callback(null, lines.filter(line => regex.test(line)).join('\n'));
-    },
+        lines.forEach((line, i) => {
+            if (regex.test(line)) {
+                line = line.substring(2);
+                this.push(i <= (lines.length - 1) ? line + '\n' : line);
+            }
+        });
+        callback();
+    }
 });
-(0, stream_1.pipeline)((0, fs_1.createReadStream)((0, path_1.join)('files', filename)), filterByRegex(/-\s(?:[\S\s]+):/), (0, fs_1.createWriteStream)('results.txt'), (err) => {
+(0, stream_1.pipeline)((0, fs_1.createReadStream)((0, path_1.join)('files', (_a = process.argv[2]) !== null && _a !== void 0 ? _a : 'style-guide.md')), splitTransform(/-\s(?:[\S\s]+):/), (0, fs_1.createWriteStream)('results.txt'), (err) => {
     if (err) {
         console.error(err);
         process.exit(1);
     }
-    console.log('The operation was a success.');
+    console.log('The operation was a success!');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
 import { createReadStream, createWriteStream } from 'fs'
-import { basename, join } from 'path'
+import { join } from 'path'
 import { pipeline, Transform } from 'stream'
 
-const getFileExtension = (filename: string) => filename.split('.').pop()?.toLowerCase()
-const filename = basename(process.argv[2] ?? '')
-
-if (!filename) {
-    console.error('Expected a filename.')
-    process.exit(1)
-}
-
-if (getFileExtension(filename) !== 'md') {
-    console.error('Only MD files are allowed.')
-    process.exit(1)
-}
-
 const filterByRegex = (regex: RegExp) => new Transform({
-    transform(chunk, encoding = 'utf8', callback) {
+    objectMode: true,
+    transform(chunk, encoding, callback) {
         const lines = (chunk + '').split('\n')
-        callback(null, lines.filter(line => regex.test(line)).join('\n'))
-    },
+
+        lines.forEach(line => {
+            if (regex.test(line)) {
+                line = line.substring(2)
+                this.push(line + '\n')
+            }
+        })
+
+        callback()
+    }
 })
 
 pipeline(
-    createReadStream(join('files', filename)),
+    createReadStream(join('files', process.argv[2] ?? 'style-guide.md')),
     filterByRegex(/-\s(?:[\S\s]+):/),
     createWriteStream('results.txt'),
     (err: any) => {
@@ -32,6 +28,6 @@ pipeline(
             process.exit(1)
         }
 
-        console.log('Created a new text file in the root directory.')
+        console.log('The operation was a success!')
     }
 )
