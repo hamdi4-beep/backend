03126c3245608bcf6afbbe41e20f63ff574d7eb8
hamdi <hamdiakippuden@gmail.com>
2025-01-23 14:01:38 +0100

    refactored the code so that it uses the pipeline function

--- dist/index.js
+++ dist/index.js
@@ -3,29 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
 const stream_1 = require("stream");
-const readStream = (0, fs_1.createReadStream)((0, path_1.join)('files', 'style-guide.md'));
-readStream
-    .on('error', console.error)
-    .pipe(createSplitTransform())
-    .pipe(createFilterTransform('##'))
-    .pipe(process.stdout);
-function createSplitTransform() {
-    return new stream_1.Transform({
-        transform(chunk, encoding, callback) {
-            for (const line of (chunk + '').split('\n'))
-                this.push(line + '\n');
-            callback();
+const createSplitTransform = () => new stream_1.Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+        for (const line of (chunk + '').split('\n'))
+            this.push(line + '\n');
+        callback();
+    }
+});
+const createFilterTransform = (searchStr) => new stream_1.Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+        if ((chunk = chunk + '').includes('https:')) {
+            const result = chunk;
+            this.push(result);
         }
-    });
-}
-function createFilterTransform(searchString) {
-    return new stream_1.Transform({
-        transform(chunk, encoding, callback) {
-            if ((chunk = chunk + '').startsWith(searchString)) {
-                const result = chunk.substring(2);
-                this.push(result);
-            }
-            callback();
-        }
-    });
-}
+        callback();
+    }
+});
+(0, stream_1.pipeline)((0, fs_1.createReadStream)((0, path_1.join)('files', 'style-guide.md')), createSplitTransform(), createFilterTransform('-'), process.stdout, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('The pipeline operation was successful');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { createReadStream, createWriteStream } from 'fs'
+import { createReadStream } from 'fs'
 import { join } from 'path'
-import { Transform } from 'stream'
-
-const readStream = createReadStream(join('files', 'style-guide.md'))
+import { pipeline, Transform } from 'stream'
 
 const createSplitTransform = () => new Transform({
-    transform(chunk, encoding, callback) {
+    objectMode: true,
+    transform(chunk: string | Buffer, encoding, callback) {
         for (const line of (chunk + '').split('\n'))
             this.push(line + '\n')
 
@@ -13,9 +12,10 @@ const createSplitTransform = () => new Transform({
     }
 })
 
-const createFilterTransform = (searchString: string) => new Transform({
-    transform(chunk: string, encoding, callback) {
-        if ((chunk = chunk + '').startsWith(searchString)) {
+const createFilterTransform = (searchStr: string) => new Transform({
+    objectMode: true,
+    transform(chunk: string | Buffer, encoding, callback) {
+        if ((chunk = chunk + '').startsWith(searchStr)) {
             const result = chunk.substring(2)
             this.push(result)
         }
@@ -24,8 +24,17 @@ const createFilterTransform = (searchString: string) => new Transform({
     }
 })
 
-readStream
-    .on('error', console.error)
-    .pipe(createSplitTransform())
-    .pipe(createFilterTransform('##'))
-    .pipe(process.stdout)
+pipeline(
+    createReadStream(join('files', 'style-guide.md')),
+    createSplitTransform(),
+    createFilterTransform('-'),
+    process.stdout,
+    (err: any) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+
+        console.log('The pipeline operation was successful')
+    }
+)
