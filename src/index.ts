596b568858b2dd24e75953a06fd467fe6957734d
hamdi <hamdiakippuden@gmail.com>
2025-01-25 07:16:42 +0100

    working with spawn function

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,22 @@
-import { createReadStream } from 'fs'
+import { spawn } from 'child_process'
+import { createWriteStream, WriteStream } from 'fs'
 import { join } from 'path'
-import { pipeline, Transform } from 'stream'
+import internal, { pipeline } from 'stream'
 
-const readStream = createReadStream(join('files', 'style-guide.md'))
+// spawns a new process with the given command
+const {stderr, stdout} = spawn('git', 'remote get-url origin'.split(' '))
 
-const createSplitTransform = (separator: string) => new Transform({
-    objectMode: true,
-    transform(chunk, encoding, callback) {
-        for (const line of (chunk + '').split(separator))
-            this.push(line + '\n')
+const stdoutStream = createWriteStream(join('logs', 'stdout.txt'))
+const stderrStream = createWriteStream(join('logs', 'stderr.txt'))
 
-        callback()
-    }
-})
+const sources = {
+    out: [stdout, stdoutStream],
+    err: [stderr, stderrStream]
+} as {
+    [x: string]: [internal.Readable, WriteStream]
+}
 
-const createFilterTransform = (searchStr: string) => new Transform({
-    objectMode: true,
-    transform(chunk, encoding, callback) {
-        if ((chunk += '').startsWith(searchStr))
-            this.push(chunk)
-
-        callback()
-    }
-})
-
-pipeline(
-    readStream,
-    createSplitTransform('\n'),
-    createFilterTransform('-'),
-    async function*(src) {
-        for await (const chunk of src) {
-            const decoded = chunk.toString()
-            console.log(decoded)
-        }
-    },
-    (err: any) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-
-        console.log('The pipeline operation was successful!')
-    }
-)
+for (const key in sources) {
+    const [std, stream] = sources[key]
+    std.pipe(stream)
+}
