9c7dd8e84082f5393561db273815fb321757550d
hamdi <hamdiakippuden@gmail.com>
2025-01-25 12:27:34 +0100

    added async iterator

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { spawn } from 'child_process'
 import { createWriteStream, WriteStream } from 'fs'
 import { join } from 'path'
-import internal, { pipeline } from 'stream'
+import internal, { pipeline, Readable } from 'stream'
 
-// spawns a new process with the given command
-const {stderr, stdout} = spawn('git', process.argv.slice(2))
+const {stdout, stderr} = spawn('git', process.argv.slice(2))
 
 const stdoutStream = createWriteStream(join('logs', 'stdout.txt'))
 const stderrStream = createWriteStream(join('logs', 'stderr.txt'))
 
-const sources = {
-    out: [stdout, stdoutStream],
-    err: [stderr, stderrStream]
-} as {
-    [x: string]: [internal.Readable, WriteStream]
-}
+const child = [
+    [stdout, stdoutStream],
+    [stderr, stderrStream]
+] as [internal.Readable, WriteStream][]
 
-for (const key in sources) {
-    const [std, stream] = sources[key]
-    std.pipe(stream)
-}
+pipeline(
+    Readable.from(child),
+    async function*(src: AsyncIterable<[internal.Readable, WriteStream]>) {
+        for await (const sub of src) {
+            const [std, stream] = sub
+            std.pipe(stream)
+        }
+    },
+    (err: any) => {
+        if (err) {
+            console.error(err)
+            return
+        }
 
-console.log('Check the "logs" directory to find the modified files.')
+        console.log('Check the logs directory for the outputs.')
+    }
+)
