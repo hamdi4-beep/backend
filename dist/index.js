13126b2616c621a98d1714d6d30ed47d9e802d4c
hamdi <hamdiakippuden@gmail.com>
2025-01-28 10:04:36 +0100

    added error handling

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
 const folders = 'parent_folder/sub_folder/sub_sub_folder'.split('/');
+const buildPath = (currFolder, i) => (0, path_1.join)(...folders.slice(0, -(folders.length - i)), currFolder);
 for (let i = 0; i < folders.length; i++) {
     const currFolder = folders[i];
-    const currPath = (0, path_1.join)(...folders.slice(0, -(folders.length - i)), currFolder);
+    const currPath = buildPath(currFolder, i);
     if ((0, fs_1.existsSync)(currPath)) {
         console.log('The path already exists!');
         break;
     }
-    (0, fs_1.mkdirSync)(currPath);
+    try {
+        (0, fs_1.mkdirSync)(currPath);
+    }
+    catch (e) {
+        console.error('Something went wrong:', e);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,20 @@ import internal from 'stream'
 
 const folders = 'parent_folder/sub_folder/sub_sub_folder'.split('/')
 
+const buildPath = (currFolder: string, i: number) =>
+    join(...folders.slice(0, -(folders.length - i)), currFolder)
+
 for (let i = 0; i < folders.length; i++) {
-    const currFolder = folders[i]
-    const currPath = join(...folders.slice(0, -(folders.length - i)), currFolder)
+    const currPath = buildPath(folders[i], i)
     
     if (existsSync(currPath)) {
         console.log('The path already exists!')
         break
     }
 
-    mkdirSync(currPath)
+    try {
+        mkdirSync(currPath)
+    } catch (e) {
+        console.error('Something went wrong:', e)
+    }
 }
