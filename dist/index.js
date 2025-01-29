e4708e5ebcde25e21efa6a8fd575b4efc33b0aaf
hamdi <hamdiakippuden@gmail.com>
2025-01-29 14:48:18 +0100

    created a function that generated a numbered array

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-console.log(createDirectory('logs'));
-function createDirectory(name) {
-    if (!(0, fs_1.existsSync)(name))
-        return (0, fs_1.mkdirSync)(name, {
-            recursive: true
-        });
-    console.log(`A directory called ${name} already exists.`);
-}
+const createList = (iteration) => iteration > 0 ? [...createList(iteration - 1), iteration] : [];
+console.log(createList(4));
--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,7 @@ import { createReadStream, createWriteStream, existsSync, mkdir, mkdirSync, Writ
 import { dirname, join } from 'path'
 import internal, { Transform } from 'stream'
 
-const path = buildPath('logs')
+const createRange = (iteration: number): number[] =>
+    iteration > 0 ? [...createRange(iteration - 1), iteration] : []
 
-const subprocess = spawn('python', [join('py', 'main.py'), ...process.argv.slice(2)])
-subprocess.on('error', console.error)
-
-const children = new Map<internal.Readable, WriteStream>([
-    [subprocess.stdout, createWriteStream(join(path, 'stdout.txt'))],
-    [subprocess.stderr, createWriteStream(join(path, 'stderr.txt'))]
-])
-
-for (const child of children) {
-    const [readableStream, writeableStream] = child
-
-    readableStream
-        .on('error', console.error)
-        .pipe(writeableStream)
-        .on('finish', () => {
-            console.log('Generated', writeableStream.path)
-            writeableStream.end()
-        })
-}
-
-function buildPath(path: string) {
-    if (!existsSync(path))
-        mkdirSync(path)
-
-    return path
-}
+console.log(createRange(5))
