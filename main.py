20227211c5065432c71d9bec02135ac5f2996ddc
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-02 11:53:49 +0100

    applied threading

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import requests
+from concurrent.futures import ThreadPoolExecutor
+import requests, threading, time
 
-def get_url(url, session):
+# creates a thread object through which we can track concurrent tasks
+local_thread = threading.local()
+
+def get_session_for_thread():
+    # returns the respective session object for each existing thread
+    if not hasattr(local_thread, 'session'):
+        # creates a session object that keeps track of relevant parameters per session.
+        local_thread.session = requests.Session()
+    return local_thread.session
+
+def fetch_site(url):
+    session = get_session_for_thread()
+
     try:
         with session.get(url) as response:
-            print(response.content)
+            print(f'Fetched the following data: \n{response.text}')
     except Exception as e:
-        print(e)
-
+        print(f'Failed to fetch site: {e}')
 
 def main():
-    with requests.Session() as s:
-        get_url("https://www.python.org/", s)
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(fetch_site, [
+            'https://realpython.com/python-concurrency/',
+            'https://realpython.com/python-gil/',
+            'https://requests.readthedocs.io/en/latest/user/advanced/',
+        ])
 
 if __name__ == '__main__':
     main()
