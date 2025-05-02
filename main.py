ab32e017e23101ccf7e6ca8a559e1bedebd89f1c
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-02 17:35:32 +0100

    fetching without threads

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from concurrent.futures import ThreadPoolExecutor
-import requests, threading, time
-
-# creates a thread object through which we can track concurrent tasks
-local_thread = threading.local()
-
-def get_session_for_thread():
-    # returns the respective session object for each existing thread
-    if not hasattr(local_thread, 'session'):
-        # creates a session object that keeps track of relevant parameters per session.
-        local_thread.session = requests.Session()
-    return local_thread.session
+import requests, time
 
 def fetch_site(url):
-    session = get_session_for_thread()
-
     try:
-        with session.get(url) as response:
-            print(f'Fetched the following data: \n{response.text}')
-    except Exception as e:
-        print(f'Failed to fetch site: {e}')
+        with requests.get(url) as response:
+            print(f'Read {len(response.content)} bytes from {url}')
+    except requests.exceptions.RequestException as e:
+        print(f'Fetching {url} failed: {e}')
+
+def fetch_all(sites):
+    for site in sites:
+        fetch_site(site)
 
 def main():
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        executor.map(fetch_site, [
-            'https://realpython.com/python-concurrency/',
-            'https://realpython.com/python-gil/',
-            'https://requests.readthedocs.io/en/latest/user/advanced/',
-        ])
+    start_time = time.perf_counter()
+
+    fetch_all([
+        'https://python.org',
+        'https://github.com',
+        'https://realpython.com/python-concurrency/',
+        'https://realpython.com/python-gil/'
+    ])
+
+    print(f'Fetched in {time.perf_counter() - start_time} seconds')
 
 if __name__ == '__main__':
     main()
