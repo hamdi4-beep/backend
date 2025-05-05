dd24c4824f5de32948b0b9f8af157f8fef364536
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-05 15:33:50 +0100

    added logic to measure time with decorators

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
-from concurrent.futures import ThreadPoolExecutor
 import requests, time
-import random
 
-MAX_WORKERS = 10
+def log_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
 
+        print(f'{func.__name__} took {time.perf_counter() - start} seconds')
+
+        return result
+
+    return wrapper
+
+@log_time
 def fetch_site(url):
-    start_time = time.perf_counter()
-
     try:
         with requests.get(url) as response:
             if not response.ok:
-                raise requests.exceptions.RequestException
-
-            time.sleep(random.random() * 10)
-
-            return f'Read {len(response.content)} bytes from {url} in {round(time.perf_counter() - start_time, 2)} seconds\n'
+                response.raise_for_status()
+            return f'Read {len(response.content)} bytes from {url}'
     except requests.exceptions.RequestException as e:
         print(f'Failed to fetch {url}: {e}')
 
-def fetch_all(urls: list):
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        return executor.map(fetch_site, urls)
-
-def save_to_file(content):
-    try:
-        with open('log.text', 'a+') as f:
-            f.write(content)
-    except Exception as e:
-        print(f'Something went wrong: {e}')
-
-def main():
-    for result in fetch_all([
-        'https://docs.python.org/3/tutorial/inputoutput.html#fancier-output-formatting',
-        'https://docs.python.org/3/tutorial/errors.html'
-    ]):
-        save_to_file(result)
-
-    print('All done!')
-
-if __name__ == '__main__':
-    main()
+ret = fetch_site('http://python.org')
+print(ret)
