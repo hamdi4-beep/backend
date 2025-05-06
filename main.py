a194cc0ff47a6104f369c5af29b888ad95dff863
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-06 10:14:37 +0100

    applied a decorator function to measure time for the fetch function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor
 import requests, time
+
+MAX_WORKERS = 10
 
 def measure_time(func):
     def wrapper(*args, **kwargs):
         start = time.perf_counter()
         result = func(*args, **kwargs)
 
-        print(f'{func.__name__} took {time.perf_counter() - start} seconds')
+        print(f'{func.__name__} with the url {args} took {time.perf_counter() - start} seconds')
 
         return result
 
@@ -13,12 +16,27 @@ def measure_time(func):
 
 @measure_time
 def fetch_site(url):
-    with requests.get(url) as response:
-        if response.raise_for_status():
-            return response.raise_for_status()
-        print(f'Read {len(response.content)} bytes from {url}')
+    try:
+        with requests.get(url) as response:
+            if response.raise_for_status():
+                print('Something went wrong')
+            return f'Read {len(response.content)} bytes from {url}'
+    except requests.exceptions.RequestException as e:
+        print(f'Failed to fetch {url}: {e}')
 
-try:
-    fetch_site('http://google.con')
-except requests.exceptions.RequestException as e:
-    print(e)
+def fetch_all(urls):
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        return executor.map(fetch_site, urls)
+
+def main():
+    urls = [
+        'https://python.org',
+        'https://realpython.com/python-sockets/',
+        'https://realpython.com/python-concurrency/'
+    ]
+
+    for ret in fetch_all(urls):
+        print(ret)
+
+if __name__ == '__main__':
+    main()
