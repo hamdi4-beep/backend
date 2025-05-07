3b8bbcf202d95eed742c07faaf15f82fe0116e4b
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-07 17:31:04 +0100

    applied threading through the submit method

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-from concurrent.futures import ThreadPoolExecutor
-import requests, time
+import concurrent.futures, requests
 
-MAX_WORKERS = 10
-
-def measure_time(func):
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-
-        print(f'{func.__name__} with the url {args} took {time.perf_counter() - start} seconds')
-
-        return result
-
-    return wrapper
-
-@measure_time
-def fetch_site(url):
-    try:
-        with requests.get(url) as response:
-            if response.raise_for_status():
-                print('Something went wrong')
+def handle_request(url):
+    with requests.get(url) as response:
+        if response.ok:
             return f'Read {len(response.content)} bytes from {url}'
-    except requests.exceptions.RequestException as e:
-        print(f'Failed to fetch {url}: {e}')
-
-def fetch_all(urls):
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        return executor.map(fetch_site, urls)
+        return f'Error status: {response.status_code} from {url}'
 
 def main():
     urls = [
-        'https://python.org',
-        'https://realpython.com/python-sockets/',
-        'https://realpython.com/python-concurrency/'
+        'https://www.google.com',
+        'https://www.yahoo.com',
+        'https://www.bing.com',
+        'https://github.com'
     ]
 
-    for ret in fetch_all(urls):
-        print(ret)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(handle_request, url) for url in urls]
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                response = future.result()
+                print(response)
+            except requests.exceptions.RequestException as e:
+                print(e)
 
 if __name__ == '__main__':
     main()
--- server/index.ts
+++ server/index.ts
@@ -1,37 +1,20 @@
 import { connect } from 'net'
-import express from 'express'
-import { createReadStream } from 'fs'
+import * as process from "node:process";
 
 const BINDING_PORT = 8000
 
-const app = express()
-app.listen(3000, () => console.log('Listening in for requests on port', 3000))
-
-app.use(express.urlencoded())
-
-app.post('/', (request, response) => {
-    const filename = Object.keys(request.body)[0]
-    
-    const socket = connect(BINDING_PORT, 'localhost', () => {
-        console.log('Connected to the remote socket.')
-        
-        createReadStream(filename)
-            .on('error', console.error)
-            .pipe(socket)
-    })
-
-    socket
-        .on('error', (err: any) => {
-            if (err.code === 'ECONNREFUSED') {
-                console.log('Make sure the listening socket is active.')
-                return
-            }
-
-            console.error(err)
-
-            response
-                .status(501)
-                .end('Something went wrong!')
-        })
-        .pipe(response)
+const socket = connect(BINDING_PORT, 'localhost', () => {
+    console.log('Connected to a remote socket.')
+    process.stdin.pipe(socket)
 })
+
+socket
+    .on('error', (err: any) => {
+        if (err.code == 'ECONNREFUSED') {
+            console.log('Check that the remote socket is actively listening for connections.')
+            return
+        }
+
+        console.error(err)
+    })
+    .pipe(process.stdout)
