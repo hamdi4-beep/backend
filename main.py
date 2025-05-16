aa3247f29c37fde763bfc316734d68cb754b3a13
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-16 10:23:56 +0100

    a handle client function now retreives the decoded data and joins them before they are displayed

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-import concurrent.futures, requests
-import time
+import socket
 
-def measure_time(func):
-    """A decorator that wraps a function around the decorated function to encapsulate the behavior-specific properties."""
-    def wrapper(*args, **kwargs):
-        """A wrapper that measures the execution time of the function."""
-        start_time = time.perf_counter()
-        # Storing the return value of the decorated function means we also have access to the returned value.
-        result = func(*args, **kwargs)
+BINDING_PORT = 8000
 
-        print(f'{func.__name__} took {time.perf_counter() - start_time} seconds')
+def handle_client(client_socket):
+    received_data = ''
 
-        return result
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data: break
+        received_data += data
 
-    return wrapper
-
-def handle_request(url):
-    """A function that takes a url and returns a response."""
-    with requests.get(url) as response:
-        if response.ok:
-            return f'Read {len(response.content)} bytes from {url}'
-        return f'Error status: {response.status_code} from {url}'
-
-@measure_time
-def fetch_all(urls):
-    """A function that takes a list of urls and returns a list of responses."""
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        # Stores a list of "future objects" which represent the execution of the submitted function.
-        futures = [executor.submit(handle_request, url) for url in urls]
-
-        for future in concurrent.futures.as_completed(futures):
-            ret = future.result()
-            print(ret)
+    print(f'Received: {received_data}')
 
 def main():
-    fetch_all(['https://www.google.com', 'https://www.yahoo.com', 'https://www.bing.com', 'https://github.com'])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('localhost', BINDING_PORT))
+
+        s.listen()
+        print("Waiting for connection...")
+
+        conn, addr = s.accept()
+        print("Connected by", addr)
+
+        handle_client(conn)
 
 if __name__ == '__main__':
     main()
--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,12 @@
+import { createReadStream } from 'fs'
 import { connect } from 'net'
 import * as process from "node:process";
 
-const BINDING_PORT = 8000
-
-const socket = connect(BINDING_PORT, 'localhost', () => {
-    console.log('Connected to a remote socket.')
-    process.stdin.pipe(socket)
-})
+const socket = connect(8000, 'localhost', () => console.log('Connected to the remote socket'))
 
 socket
-    .on('error', (err: any) => {
-        if (err.code == 'ECONNREFUSED') {
-            console.log('Check that the remote socket is actively listening for connections.')
-            return
-        }
-
-        console.error(err)
+    .on('error', console.error)
+    .on('connect', () => {
+        const readStream = createReadStream('package.json').on('error', console.error)
+        readStream.pipe(socket)
     })
-    .pipe(process.stdout)
