bb45475761a8701546f31bf83730e8fa8df6e65f
hamdi4-beep <hamdiakippuden@gmail.com>
2025-05-29 07:47:23 +0100

    added some docstrings to improve readability.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import concurrent.futures, requests, time
 
 def measure_time(func):
+    """A decorator that measures the execution time of a function."""
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
@@ -12,25 +13,24 @@ def measure_time(func):
     return wrapper
 
 def fetch_url(url):
+    """A function that makes a network request."""
     with requests.get(url) as response:
         if not response.ok:
             return response.status_code
+
         return f'Read {len(response.content)} bytes from {url}'
 
 @measure_time
 def fetch_all(urls):
-    results = []
-
+    """A function that fetches a list of urls concurrently."""
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         for future in concurrent.futures.as_completed([executor.submit(fetch_url, url) for url in urls]):
-            results.append(future.result())
-
-    return results
+            response = future.result()
+            print(response)
 
 def main():
     try:
-        response = fetch_all(['https://python.org', 'https://github.com'])
-        print(response)
+        fetch_all(['https://python.org', 'https://github.com'])
     except Exception as e:
         print(f'Something went wrong: {e}')
 
