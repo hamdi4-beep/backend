dcf203b9d11d01580fb44f1d6ff78755a2f4da9b
hamdi4-beep <hamdiakippuden@gmail.com>
2025-06-11 12:00:39 +0100

    added a decorator that measures a function's execution time

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import time
+
 import requests, concurrent.futures
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+
+        print(f'{func.__name__} took {time.perf_counter() - start} seconds')
+
+    return wrapper
 
 def fetch_url(url):
     with requests.get(url) as response:
         if response.ok:
             return f'Read {len(response.content)} bytes from {url}'
 
+@measure_time
 def fetch_all(urls: list):
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         for future in [executor.submit(fetch_url, url) for url in urls]:
@@ -13,7 +25,7 @@ def fetch_all(urls: list):
 
 def main():
     try:
-        fetch_all(['https://python.org', 'https://github.com'])
+        fetch_all(['https://www.python.org', 'https://www.github.com'])
     except Exception as e:
         print(f'Something went wrong: {e}')
 
