fe69d1ba5cde1439d3ac94a7f679a0d4d111df08
hamdi4-beep <hamdiakippuden@gmail.com>
2025-06-11 11:52:44 +0100

    a basic get request in Python

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
-import concurrent.futures, requests, time
+import requests
 
-def measure_time(func):
-    """A decorator that measures the execution time of a function."""
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-
-        print(f'{func.__name__} took {time.perf_counter() - start_time} seconds')
-
-        return result
-
-    return wrapper
-
-def fetch_url(url):
-    """A function that makes a network request."""
+def handle_request(url):
     with requests.get(url) as response:
-        if not response.ok:
-            return response.status_code
-
-        return f'Read {len(response.content)} bytes from {url}'
-
-@measure_time
-def fetch_all(urls):
-    """A function that fetches a list of urls concurrently."""
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        for future in concurrent.futures.as_completed([executor.submit(fetch_url, url) for url in urls]):
-            response = future.result()
-            print(response)
+        if response.ok:
+            return f'Read {len(response.content)} bytes from {url}'
 
 def main():
     try:
-        fetch_all(['https://python.org', 'https://github.com'])
+        result = handle_request("htp://python.org")
+        print(result)
     except Exception as e:
         print(f'Something went wrong: {e}')
 
--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,7 @@
-import { createReadStream } from 'fs'
-import { connect } from 'net'
+import {connect} from 'net'
 
-const BINDING_PORT = 8000
+const socket = connect(8000, 'localhost')
 
-const socket = connect(BINDING_PORT, 'localhost', () => {
-    console.log('Connected to the remote socket.')
-    sendFile('tsconfig.json')
-})
-
-socket.on('error', (err: any) => {
-    if (err.code == 'ECONNREFUSED') {
-        console.log('The remote socket is not currently listening.')
-        return
-    }
-
-    console.error(err)
-})
-
-function sendFile(filename: string) {
-    return createReadStream(filename)
-        .on('error', console.error)
-        .pipe(socket)
-        .on('end', () => console.log('Finished sending over the file.'))
-}
+socket
+    .on('error', console.error)
+    .on('connect', () => console.log('Connected to the listening socket.'))
