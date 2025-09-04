882fd88f328d2503969284fe1257a762ab54ac21
hamdi4-beep <hamdiakippuden@gmail.com>
2025-09-04 13:52:20 +0100

    configured an arcjet file

--- config/env.ts
+++ config/env.ts
@@ -4,4 +4,10 @@ config({
     path: `.env.${process.env.NODE_ENV || 'development'}.local`
 })
 
-export const {PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN} = process.env
+export const {
+    PORT,
+    DB_URI,
+    JWT_SECRET,
+    JWT_EXPIRES_IN,
+    ARCJET_KEY
+} = process.env
