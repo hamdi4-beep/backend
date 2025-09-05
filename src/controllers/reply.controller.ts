7ef6473163908eec8388432b4c0576e41f439b1c
hamdi4-beep <hamdiakippuden@gmail.com>
2025-09-05 14:56:52 +0100

    create a controller method to add new replies

--- src/controllers/reply.controller.ts
+++ src/controllers/reply.controller.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction } from 'express'
 import Reply from '../models/reply.model'
+import mongoose from 'mongoose'
 
 export const getReplies = async (request: express.Request, response: express.Response, next: NextFunction) => {
     try {
@@ -17,6 +18,28 @@ export const getReplies = async (request: express.Request, response: express.Res
                 success: true,
                 replies
             })
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const createReply = async (request: express.Request, response: express.Response, next: NextFunction) => {
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    try {
+        const newReplies = await Reply.create([{
+            ...request.body
+        }], {session})
+
+        response
+            .status(201)
+            .json({
+                success: true,
+                reply: newReplies[0]
+            })
+
+        await session.commitTransaction()
     } catch (err) {
         next(err)
     }
--- src/routes/reply.routes.ts
+++ src/routes/reply.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getReplies } from "../controllers/reply.controller";
+import { createReply, getReplies } from "../controllers/reply.controller";
 
 const replyRouter = Router()
 
 replyRouter.get('/', getReplies)
+replyRouter.post('/create-reply', createReply)
 
 export default replyRouter
